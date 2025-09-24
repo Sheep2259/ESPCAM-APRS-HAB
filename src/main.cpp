55b11995c704c9c0905df84f7cd2cfca3525ef6c
Sheep2259 <isaacmoon10@gmail.com>
2025-09-24 17:54:45 +0100

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 
 void setup() {
   Serial.begin(115200);
+  Serial.print("test")
 
 }
 
@@ -21,16 +22,17 @@ void loop() {
   char destination[] = "APRS";
   char latitude[] = "4911.67N";
   char longitude[] = "01635.96E";
-  char message[] = "vscode test DATATEST AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+  char messagelora[] = "lora transmission";
+  char message2m[] = "2m transmission";
 
   
   for (int i = 0; i < 1; i++) {
-    transmit_2m(destination, latitude, longitude, message);
+    transmit_2m(destination, latitude, longitude, message2m);
     delay(2000);
   }
 
   for (int i = 0; i < 1; i++) {
-    transmit_lora(destination, latitude, longitude, message);
+    transmit_lora(destination, latitude, longitude, messagelora);
     delay(2000);
   }
 
--- src/radio.cpp
+++ src/radio.cpp
@@ -16,9 +16,10 @@
 void transmit_2m(char destination[], char latitude[], char longitude[], char message[]){
 
   // initialize SX1278 for 2m APRS
-  Serial.print(F("[SX1278] Initializing ... "));
+  // Serial.print(F("[SX1278] Initializing ... "));
   int beginfskstate = radio.beginFSK(144.8);
 
+  /*
   if(beginfskstate == RADIOLIB_ERR_NONE) {
     Serial.println(F("success!"));
   } else {
@@ -26,13 +27,15 @@ void transmit_2m(char destination[], char latitude[], char longitude[], char mes
     Serial.println(beginfskstate);
     while (true) { delay(10); }
   }
+  */
 
   // initialize AX.25 client
-  Serial.print(F("[AX.25] Initializing ... "));
+  // Serial.print(F("[AX.25] Initializing ... "));
   // source station callsign:     "M7CWV"
   // source station SSID:         0
   // preamble length:             8 bytes
   int beginax25state = ax25.begin("M7CWV");
+  /*
   if(beginax25state == RADIOLIB_ERR_NONE) {
     Serial.println(F("success!"));
   } else {
@@ -40,11 +43,13 @@ void transmit_2m(char destination[], char latitude[], char longitude[], char mes
     Serial.println(beginax25state);
     while (true) { delay(10); }
   }
+  */
 
   // initialize APRS client
-  Serial.print(F("[APRS] Initializing ... "));
+  // Serial.print(F("[APRS] Initializing ... "));
   // symbol:         '>' (car)
   int aprsbeginstate = aprs.begin('>');
+  /*
   if(aprsbeginstate == RADIOLIB_ERR_NONE) {
     Serial.println(F("success!"));
   } else {
@@ -52,12 +57,12 @@ void transmit_2m(char destination[], char latitude[], char longitude[], char mes
     Serial.println(aprsbeginstate);
     while (true) { delay(10); }
   }
+  */
 
 
 
-
-  Serial.println(F("[APRS] Sending location reports"));
   /*
+  Serial.println(F("[APRS] Sending location reports"));
   // send a location with message
   char destination[] = "APRS";
   char latitude[] = "4911.67N";
@@ -66,18 +71,12 @@ void transmit_2m(char destination[], char latitude[], char longitude[], char mes
   */
   int sendstate = aprs.sendPosition(destination, 0, latitude, longitude, message);
 
-  Serial.println(F("[APRS] All done!"));
+  // Serial.println(F("[APRS] All done!"));
 }
-
-
-
-
-
 
 
 void transmit_lora(char destination[], char latitude[], char longitude[], char message[]){
 
-  
   // initialize SX1278 with the settings necessary for LoRa iGates
   Serial.print(F("[SX1278] Initializing ... "));
   // frequency:                   433.775 MHz
@@ -89,7 +88,7 @@ void transmit_lora(char destination[], char latitude[], char longitude[], char m
   // when using one of the non-LoRa modules for AX.25
   // (RF69, CC1101, Si4432 etc.), use the basic begin() method
   // int state = radio.begin();
-
+  /*
   if(radiobeginstate == RADIOLIB_ERR_NONE) {
     Serial.println(F("success!"));
   } else {
@@ -97,14 +96,15 @@ void transmit_lora(char destination[], char latitude[], char longitude[], char m
     Serial.println(radiobeginstate);
     while (true) { delay(10); }
   }
-
+  */
   // initialize APRS client
   Serial.print(F("[APRS] Initializing ... "));
   // symbol:                      '>' (car)
   // callsign                     "N7LEM"
   // SSID                         1
-  char source[] = "N7LEM";
+  char source[] = "M7CWV";
   int loraaprsstate = loraaprs.begin('>', source, 1);
+  /*
   if(loraaprsstate == RADIOLIB_ERR_NONE) {
     Serial.println(F("success!"));
   } else {
@@ -112,11 +112,11 @@ void transmit_lora(char destination[], char latitude[], char longitude[], char m
     Serial.println(loraaprsstate);
     while (true) { delay(10); }
   }
+  */
 
 
 
-
-  Serial.print(F("[APRS] Sending position ... "));
+  //Serial.print(F("[APRS] Sending position ... "));
 
   /*
   // send a location with message
@@ -128,11 +128,12 @@ void transmit_lora(char destination[], char latitude[], char longitude[], char m
   */
   int state = loraaprs.sendPosition(destination, 1, latitude, longitude, message);
 
-
+  /*
   if(state == RADIOLIB_ERR_NONE) {
     Serial.println(F("success!"));
   } else {
     Serial.print(F("failed, code "));
     Serial.println(state);
   }
+  */
 }
